ff9a5296ad4b859c97f9005ad1c764ffee55a32f
PaulZC <pc235603@gmail.com>
2021-08-02 09:42:57 +0100

    Correcting I2C restart-after-power-down problems

--- Firmware/smol_Power_Board/Tiny_AVR_Programmer.bat
+++ Firmware/smol_Power_Board/Tiny_AVR_Programmer.bat
@@ -2,7 +2,7 @@
 @pause
 :loop
 @echo Flashing bootloader and  firmware...
-@avrdude -C avrdude.conf -ptattiny841 -cusbtiny -e -Uefuse:w:0xFF:m -Uhfuse:w:0xDF:m -Ulfuse:w:0x62:m -Uflash:w:smol_Power_Board_LiPo_ATtiny841.ino.hex:i
+@avrdude -C avrdude.conf -ptattiny841 -cusbtiny -e -Uefuse:w:0xFF:m -Uhfuse:w:0xDF:m -Ulfuse:w:0x42:m -Uflash:w:smol_Power_Board_LiPo_ATtiny841.ino.hex:i
 @echo Done programming! Move on to the next board.
 @pause
 goto loop
